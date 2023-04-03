da0b4fb24a93758a7dc2e885730772dd96783fc2
soniangn <nguyen.sonia@gmail.com>
2023-04-03 15:56:56 -0700

    display html with cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """ display a HTML page with list of cities 
+    sorted by states
+    """
+    cities = storage.all(City)
+    cities_list = sorted(cities.values(), key=lambda x: x.name)
+    return render_template("8-cities_by_states.html", cities=cities_list)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """ remove the current SQLalchemy session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
