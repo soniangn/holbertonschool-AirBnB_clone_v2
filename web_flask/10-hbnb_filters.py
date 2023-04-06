b63cc62ebe53c646d69c3cf0f01e86528c21f449
soniangn <nguyen.sonia@gmail.com>
2023-04-06 14:38:36 -0700

    done task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
-""" doc """
+"""
+    Create Flask instance
+"""
+from flask import Flask
+from flask import render_template
 from models import storage
-from flask import Flask, render_template
 from models.state import State
-from models.city import City
+from models.amenity import Amenity
+
 
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
+@app.route('/hbnb_filters', strict_slashes=False, methods=["GET"])
 def hbnb_filters():
-    """ display a HTML page """
+    """
+        display a HTML page
+    """
     states = storage.all(State)
     amenities = storage.all(Amenity)
-    return render_template(states=states, amenities=amenities)
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
-def close(exception):
+def close_session(exception):
+    """ remove the current SQLalchemy session """
     storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
